74edd3441202fdcffa3b8debaeadbc86e9e7f61f
Nikhil4693 <nikhil.wadatkar4693@gmail.com>
2023-01-27 22:08:35 +0530

    file added

--- src/main/java/com/bs/milestone1/lecture5/AlphaPattern.java
+++ src/main/java/com/bs/milestone1/lecture5/AlphaPattern.java
@@ -8,7 +8,7 @@ public class AlphaPattern {
         int num = s.nextInt();
         int i = 1;
         while (i <= num) {
-            int j = 1;
+            int j = 0;
             while (j < i) {
                 System.out.print((char) ('A' + i));
                 j = j + 1;
